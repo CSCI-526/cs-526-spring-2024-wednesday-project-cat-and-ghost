6e777f2dde0a87140de9b5f94d79bba46c2acf17
RuijieWangg <ruijiew@usc.edu>
2024-03-25 16:44:37 -0700

    update tutorial(press space to change color)

--- Assets/Scripts/ToggleObjects.cs
+++ Assets/Scripts/ToggleObjects.cs
@@ -4,14 +4,39 @@ public class ToggleObjects : MonoBehaviour {
     public GameObject currentObject; // 当前要关闭的GameObject
     public GameObject nextObject;    // 要显示的下一个GameObject
 
+    public GameObject showObject;    // 要显示的下一个GameObject
+    public bool isFirst;
+    bool isKeyPress = false;
+
     // 此方法应该被绑定到按钮的OnClick事件
     public void OnButtonClick() {
-        if (currentObject != null) {
-            currentObject.SetActive(false); // 关闭当前GameObject
-        }
+        if(isFirst) {
+            if(isKeyPress) {
+                showObject.SetActive(false);
 
-        if (nextObject != null) {
-            nextObject.SetActive(true); // 显示下一个GameObject
+                if (currentObject != null) {
+                    currentObject.SetActive(false); // 关闭当前GameObject
+                }
+
+                if (nextObject != null) {
+                    nextObject.SetActive(true); // 显示下一个GameObject
+                }
+            }
+
+        } else {
+            if (currentObject != null) {
+                currentObject.SetActive(false); // 关闭当前GameObject
+            }
+
+            if (nextObject != null) {
+                nextObject.SetActive(true); // 显示下一个GameObject
+            }
+        }
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            isKeyPress = true;
         }
     }
 }
